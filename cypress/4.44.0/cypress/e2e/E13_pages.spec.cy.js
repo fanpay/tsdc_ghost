4afc611496f64f21f4b5e15d6657b522b087faf6
Johana <j.ojedaa@uniandes.edu.co>
2023-05-10 21:36:32 -0500

    Se agrega funcionalidad screenshots

--- cypress/4.44.0/cypress/e2e/E13_pages.spec.cy.js
+++ cypress/4.44.0/cypress/e2e/E13_pages.spec.cy.js
@@ -16,11 +16,11 @@
         let email = Cypress.config("email");
         let password = Cypress.config("password");
         cy.get('input[name="identification"]').type(email)
-  
+        cy.screenshot(folderPathSS);
         cy.get('input[name="password"]').type(password)
-      
+        cy.screenshot(folderPathSS);
         cy.get('.login.gh-btn.gh-btn-login.gh-btn-block.gh-btn-icon.js-login-button.ember-view').click()
-        
+        cy.screenshot(folderPathSS);
       })
       cy.wait(1000)
   
@@ -31,26 +31,27 @@
       // When
       
       cy.get(".gh-nav-top").contains("Pages").click();
-     
+      cy.screenshot(folderPathSS);
       cy.get('.ember-view.gh-btn.gh-btn-primary.view-actions-top-row').click();
+      cy.screenshot(folderPathSS);
       cy.get('.gh-editor-title.ember-text-area.gh-input.ember-view').type("Página escenario 4");
-            
+      cy.screenshot(folderPathSS);
       cy.get('.settings-menu-toggle.gh-btn.gh-btn-editor.gh-btn-icon.icon-only.gh-btn-action-icon').click();
       cy.wait(1000)
-     
+      cy.screenshot(folderPathSS);
       cy.get('.gh-date-time-picker-date').clear()
-    
+      cy.screenshot(folderPathSS);
       cy.get('.gh-date-time-picker-date').type("2023-05-03");
-    
+      cy.screenshot(folderPathSS);
       cy.get('.settings-menu-toggle.gh-btn.gh-btn-editor.gh-btn-icon.icon-only.gh-btn-action-icon').click();
-      
+      cy.screenshot(folderPathSS);
       
       cy.get(".ember-view.ember-basic-dropdown-trigger.gh-btn.gh-btn-editor.gh-publishmenu-trigger").click();
-      
+      cy.screenshot(folderPathSS);
       cy.get('.gh-btn.gh-btn-black.gh-btn-icon.ember-view').click();
        //then
       cy.get('.gh-notification-actions > a').click();
-
+      cy.screenshot(folderPathSS);
       
      
     });
--- cypress/4.44.0/cypress/e2e/E15_tags.spec.cy.js
+++ cypress/4.44.0/cypress/e2e/E15_tags.spec.cy.js
@@ -22,11 +22,11 @@ describe('Crear un tag con clasificación interna', () => {
       let email = Cypress.config("email");
       let password = Cypress.config("password");
       cy.get('input[name="identification"]').type(email)
-    
+      cy.screenshot(folderPathSS);
       cy.get('input[name="password"]').type(password)
-   
+      cy.screenshot(folderPathSS);
       cy.get('.login.gh-btn.gh-btn-login.gh-btn-block.gh-btn-icon.js-login-button.ember-view').click()
-    
+      cy.screenshot(folderPathSS);
     })
     cy.wait(1000)
 
@@ -35,47 +35,48 @@ describe('Crear un tag con clasificación interna', () => {
 
     // When
     cy.get(".gh-nav-top").contains("Tags").click();
- 
+    cy.screenshot(folderPathSS);
     cy.get('.ember-view.gh-btn.gh-btn-primary').click();
-    
+    cy.screenshot(folderPathSS);
 
     cy.get('#tag-name').type(tag);
-   
+    cy.screenshot(folderPathSS);
     cy.get('.gh-btn.gh-btn-primary.gh-btn-icon.ember-view').click();
     cy.wait(500);
     
     cy.go('back');
-   
+    cy.screenshot(folderPathSS);
     cy.go('back');
  
     cy.get(".gh-nav-top").contains("Posts").click();
     cy.wait(1000);
+    cy.screenshot(folderPathSS);
     cy.get(".gh-content-status-scheduled.gh-badge.nowrap").click();
-    
+    cy.screenshot(folderPathSS);
     //cy.get('.gh-canvas').contains(" ").click();
 
     cy.get('.settings-menu-toggle.gh-btn.gh-btn-editor.gh-btn-icon.icon-only.gh-btn-action-icon').click();
     cy.wait(1000)
-    
+    cy.screenshot(folderPathSS);
     cy.get('#tag-input').type(tag)
- 
+    cy.screenshot(folderPathSS);
     cy.get('[aria-current="true"]').click();
- 
+    cy.screenshot(folderPathSS);
     
     cy.wait(4000)
     cy.go('back');
-    
+    cy.screenshot(folderPathSS);
 
     // Then
     cy.wait(2000);
     cy.get('.gh-btn-red > span').click()
     cy.wait(2000);
-    
+    cy.screenshot(folderPathSS);
     cy.get(".gh-content-status-scheduled.gh-badge.nowrap").click();
-    
+    cy.screenshot(folderPathSS);
     cy.get('.settings-menu-toggle.gh-btn.gh-btn-editor.gh-btn-icon.icon-only.gh-btn-action-icon').click();
     cy.wait(1000)
-   
+    cy.screenshot(folderPathSS);
   });
 
 })
